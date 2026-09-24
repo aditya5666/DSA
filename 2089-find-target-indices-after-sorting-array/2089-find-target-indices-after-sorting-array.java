5e55cc3ab024c3ad9cbfbdb454a54aacd3a4e139
Aditya Gupta <datarmgupta@gmail.com>
2026-09-24 10:53:20 +0530

    Time: 5 ms (72.7%), Space: 44.9 MB (72.67%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -5,64 +5,9 @@ class Solution {
 
         List<Integer> ans = new ArrayList<>();
 
-        int first = findFirst(nums, target);
-        int last = findLast(nums, target);
-
-        if (first == -1) {
-            return ans;
-        }
-
-        for (int i = first; i <= last; i++) {
-            ans.add(i);
-        }
-
-        return ans;
-    }
-
-    public int findFirst(int[] nums, int target) {
-
-        int low = 0;
-        int high = nums.length - 1;
-        int ans = -1;
-
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                high = mid - 1;
-            }
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-
-    public int findLast(int[] nums, int target) {
-
-        int low = 0;
-        int high = nums.length - 1;
-        int ans = -1;
-
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                low = mid + 1;
-            }
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                ans.add(i);
             }
         }
 
